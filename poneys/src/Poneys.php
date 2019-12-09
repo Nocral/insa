16828b8f2c3893cc0e98b9de2e30a0fe763ec1ce
Flavien <flavien.ehret@insa-cvl.fr>
2019-12-09 16:36:52 +0100

    rien

--- poneys/src/Poneys.php
+++ poneys/src/Poneys.php
@@ -6,14 +6,20 @@ class Poneys
 {
     private $count = 8;
 
+
     /**
      * Retourne le nombre de poneys
      *
      * @return void
      */
-    public function getCount(): int
+    public function getCount()
     {
         return $this->count;
+    }
+
+    public function setCount(int $number)
+    {
+	$this->count = $number;
     }
 
     /**
@@ -23,9 +29,26 @@ class Poneys
      *
      * @return void
      */
-    public function removePoneyFromField(int $number): void
+    public function removePoneyFromField(int $number) 
     {
+	if($number>$this->count)
+		throw new InvalidArgumentException('total <0 :'.$number);
+	if($number<0)
+		throw new InvalidArgumentException('number <0 :'.$number);
         $this->count -= $number;
+    }
+
+    public function addPoneyToField(int $number)
+    {
+	$this->count += $number;
+    }
+
+
+    public function haveSpace(int $taille)
+    {
+	if($this->count<$taille)
+		return True;
+	return False;
     }
 
     /**
@@ -33,7 +56,7 @@ class Poneys
      *
      * @return array
      */
-    public function getNames(): array
+    public function getNames()
     {
 
     }
--- poneys/tests/PoneysTest.php
+++ poneys/tests/PoneysTest.php
@@ -8,21 +8,89 @@ require_once 'src/Poneys.php';
  */
 class PoneysTest extends TestCase
 {
+
+    //private $poneys = NULL;
+    public function setUp()
+    {
+	$this->poneys = new Poneys();
+	$this->poneys->setCount(qtPoney);
+	print('ouééééééé ');
+    }
+
+    public function tearDown()
+    {
+	$this->poneys = NULL;
+	print('terminated');
+    }
+
     /**
      * Undocumented function
      *
+     * @dataProvider provider
+     *
      * @return void
      */
-    public function testRemovePoneyFromField()
+    public function testRemovePoneyFromField(int $number, int $expected)
     {
         // Setup
         $Poneys = new Poneys();
 
         // Action
-        $Poneys->removePoneyFromField(3);
+        $Poneys->removePoneyFromField($number);
 
         // Assert
-        $this->assertEquals(5, $Poneys->getCount());
+	$this->assertEquals($expected, $Poneys->getCount());
+
     }
+
+    public function testException()
+    {
+	$Poneys = new Poneys();
+	$this->expectException(InvalidArgumentException::class);
+	$Poneys->removePoneyFromField(9);
+	$Poneys->removePoneyFromField(-1);
+    }
+
+
+    public function testgetNames()
+    {
+	$names = ['Twilight Sparkle','Rainbow Dash', 'Rarity', 'Applejack'];
+	$this->poneys = $this->getMockBuilder('Poneys')->getMock();
+	$this->poneys
+		->expects($this->exactly(1))
+		->method('getNames')
+		->willReturn($names);
+	$this->assertEquals($names, $this->poneys->getNames());
+    }
+
+    public function testAddPoneyToField()
+    {
+	// Setup
+	$Poneys = new Poneys();
+	  
+	// Action
+	$Poneys->addPoneyToField(3);
+
+	//Assert
+	$this->assertEquals(11, $Poneys->getCount());
+    }
+
+    public function testhaveSpace()
+    {
+	$Poneys = new Poneys();
+	$this->assertTrue($Poneys->haveSpace(taillechamps));
+	$Poneys->addPoneyToField(10);
+	$this->assertFalse($Poneys->haveSpace(taillechamps));
+    }
+
+
+    public function provider()
+    {
+	 return [
+		 [3,5],
+		 [6,2]
+	];
+    }
+
 }
 ?>
