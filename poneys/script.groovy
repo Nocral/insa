86d54027e82b1953fbed84fd036ead8ae59eea98
root <root@moi.cri15.ensi-bourges.fr>
2019-12-10 16:48:25 +0100

    scriptforrrzeaze

---
+++ poneys/script.groovy
@@ -0,0 +1,34 @@
+pipeline {
+    agent any
+    
+   stages {
+      stage('checkout') {
+         steps {
+            git 'https://github.com/Nocral/insa.git'
+         }
+      }
+      
+      stage('setup') {
+         steps {
+            dir('poneys') {
+                sh './install_composer.sh'
+                sh 'php composer.phar install'
+            }
+         }
+      }
+      
+      stage('tests') {
+          steps {
+              dir('poneys') {
+                sh './vendor/bin/phpunit tests/PoneysTest.php'
+            }
+              xunit (
+                tresholds: [
+                    skipped(failuresThreshold:'0'),
+                    failed(failuresThreshold:'0')
+                ],
+                tools: [ PHPUnit(pattern: 'poneys/logfile.xml') ])
+          }
+      }
+    }
+}
